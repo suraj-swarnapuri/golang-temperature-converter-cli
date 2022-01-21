926b71a5ec6d99198c12ab2d5a3c893423b68afc
Suraj Swarnapuri <suraj-swarnapuri@github.com>
2022-01-21 14:03:46 -0600

    fix test

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ func main() {
 		if err != nil {
 			printError(errReadingInput)
 		}
-		if shouldConvertAgain != "Y" {
+		if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y" {
 			fmt.Println("Good bye!")
 			break
 		}
